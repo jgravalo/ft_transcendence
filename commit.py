1e4978e47760e35f9c4246e1fd818dd528ae6f80
Jesús alberto Grávalos olivier <jgravalo@car14s4.42barcelona.com>
2024-11-26 18:10:10 +0100

    commit.py

--- commit.py
+++ commit.py
@@ -1,14 +1,18 @@
 import os
-
+import sys
+#test
 # Ruta al programa ejecutable (por ejemplo, /bin/ls en sistemas Unix)
-programa = "/bin/ls"
+programa = "/usr/bin/git"
 
 # Argumentos para el programa (el primero suele ser el nombre del programa)
-args = {["git", "add", "."], ["git", "commit", "-m", sys.argv[1]], ["git", "push"]}
+args = [["git", "add", "."], ["git", "commit", "-m", sys.argv[0]], ["git", "push"]]
 
 # Variables de entorno (puedes copiar las existentes con os.environ)
 entorno = os.environ.copy()
 
 # Ejecutar el programa
-for x < 3:
-	os.execve(args, args, entorno)
+for i in range(3):
+	pid = os.fork()
+	if pid == 0:
+		os.execve(programa, args[i], entorno)
+	os.wait()
