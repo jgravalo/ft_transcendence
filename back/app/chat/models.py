5c058e50160fef3bea520a5efe2550372ff59172
jgravalo <jesusgrawork361@gmail.com>
2025-02-28 23:13:03 +0100

    store chat

--- back/app/chat/models.py
+++ back/app/chat/models.py
@@ -6,12 +6,12 @@ from users.models import User
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
+    message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username}: {self.content[:20]}"
 
-# class Group(models.Model):
-#     room = models.TextField()
-#     history = models.ManyToManyField(Message, symmetrical=False, related_name='message_for', blank=True)
+class Group(models.Model):
+    room = models.TextField()
+    history = models.ManyToManyField(Message, symmetrical=False, related_name='message_for', blank=True)
