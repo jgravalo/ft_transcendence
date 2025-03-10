32301905b187ed5ceef848fe5aea77a43da454f0
jgravalo <jesusgrawork361@gmail.com>
2025-03-10 19:44:39 +0100

    first commit

--- back/app/app/settings.py
+++ back/app/app/settings.py
@@ -229,8 +229,8 @@ REST_FRAMEWORK = {
 # Configuración de SimpleJWT
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=5),#timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=12),#timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=5), #timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1), #timedelta(seconds=12),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
