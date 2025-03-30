c1769600eab4100a52199e8428d4f08e6788f8c2
Victor Aguilar <veaz.24@gmail.com>
2025-03-30 21:57:17 +0200

    cookies deleted from websockets

--- back/app/app/asgi.py
+++ back/app/app/asgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from .token_auth_middleware import TokenAuthMiddleware
 import game.routing
 import users.routing
 import chat.routing
@@ -23,7 +23,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": TokenAuthMiddleware(
         URLRouter(
             game.routing.websocket_urlpatterns +
             users.routing.websocket_urlpatterns +
